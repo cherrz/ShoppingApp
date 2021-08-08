372f4b3d73f001b7bf89016f84f1242024eb1eee
cherrz <dwiszniewski94@gmail.com>
2021-08-08 23:06:30 +0200

    adding some logic to the app

--- main.js
+++ main.js
@@ -3,9 +3,35 @@
 
 const addTaskButton = document.querySelector('button.addTaskButton');
 const inputValue = document.querySelector('input.entryTask');
+const ultoDoList = document.querySelector('ul.todolist');
+const toDoArray = [];
 
-console.log(inputValue);
+// localStorage.clear();
 
-addTaskButton.addEventListener('click', function(){
-    console.log(inputValue.value); 
-})
+addTaskButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    const li = document.createElement('li');
+    const checkedIcon = document.createElement('i');
+    checkedIcon.classList.add('icon-ok-outline');
+    if (inputValue.value !== '') {
+        const task = inputValue.value;
+        li.innerHTML = task;
+        toDoArray.push(task)
+        saveToLocalStorage(toDoArray);
+        inputValue.value = '';
+        ultoDoList.appendChild(li);
+        li.appendChild(checkedIcon);
+        checkedIcon.addEventListener('click', function () {
+            checkedIcon.parentElement.style.textDecoration = 'line-through';
+            checkedIcon.remove();
+        })
+    } else {
+        alert('Nie wpisałeś nazwy produktu!');
+    }
+});
+
+function saveToLocalStorage(toDoArray){
+    localStorage.setItem('ToDo', toDoArray);
+    console.log(localStorage.getItem('toDo'));
+}
+
